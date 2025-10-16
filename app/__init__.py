b073aeab322ed537c4c0acda76422df4fd5a3776
Diogo-creator <diogolucas143@gmail.com>
2025-10-16 20:55:20 -0300

    Controle de Login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
+from flask_login import LoginManager
+import flask_bcrypt as Bcrypt
 
 load_dotenv('.env')
 
@@ -15,6 +17,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+LoginManager = LoginManager(app)
+LoginManager.login_view = 'login'
 
 from app.views import homepage
 from app.models import Contato
--- app/models.py
+++ app/models.py
@@ -1,6 +1,20 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    last_name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+
+    def get_id(self):
+        return self.id
+    
 class Contato(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data_envio = db.Column(db.DateTime(), default=datetime.now)
