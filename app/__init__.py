116468153741fa3648e12856e6231ec67948e1ff
Diogo-creator <diogolucas143@gmail.com>
2025-10-16 20:17:23 -0300

    Variável de Ambiente e Python-Dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATA_BASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'minha_senha_secreta'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
---
+++ app/create_secret.py
@@ -0,0 +1,5 @@
+import secrets
+
+sc = secrets.token_urlsafe(40)
+
+print(sc)
